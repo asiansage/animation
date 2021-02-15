da01556072af2940b3dfd6d35a4da5e1e62c22db
asiansage <74437698+asiansage@users.noreply.github.com>
2021-02-15 13:40:56 -0500

    Created

---
+++ infinitepong.py
@@ -0,0 +1,129 @@
+import os
+from time import sleep
+
+score1 = 0
+score2 = 0
+
+while True:
+    #1
+    print("|o           |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #2
+    print("|-o          |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #3
+    print("|--o         |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #4
+    print("|---o        |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #5
+    print("|----o       |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #6
+    print("|-----o      |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #7
+    print("|------o     |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #8
+    print("|-------o    |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #9
+    print("|--------o   |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #10
+    print("|---------o  |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #11
+    print("|----------o |")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #12
+    print("|-----------o|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #13
+    print("|           o|")
+    score2 += 1
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R1
+    print("|          o-|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R2
+    print("|         o--|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R3
+    print("|        o---|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R4
+    print("|       o----|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R5
+    print("|      o-----|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R6
+    print("|     o------|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R7
+    print("|    o-------|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R8
+    print("|   o--------|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R9
+    print("|  o---------|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R10
+    print("| o----------|")
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #R11
+    print("|o-----------|")
+    score1 += 1
+    print(str(score1) + "-" + str(score2))
+    sleep(0.07)
+    os.system('cls' if os.name == 'nt' else 'clear')
